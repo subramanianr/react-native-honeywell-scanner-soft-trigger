f2727ab46245880066818cfcf3e704eb57a53cd1
Subramanian Ramaswamy <Subramanian.Ramaswamy@ingrammicro.com>
2022-08-12 18:11:39 -0700

    Added Symbology CodeId mapper to send at success event

--- android/src/main/java/sr/scanner/HoneywellScanner/HoneywellScannerModule.java
+++ android/src/main/java/sr/scanner/HoneywellScanner/HoneywellScannerModule.java
@@ -1,5 +1,6 @@
 package sr.scanner.HoneywellScanner;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,6 +39,86 @@ public class HoneywellScannerModule extends ReactContextBaseJavaModule implement
     private AidcManager manager;
     private BarcodeReader reader;
     private ReactApplicationContext mReactContext;
+    private static final Map<String, String> symbologyCodeMapper = createMap();
+
+    private static Map<String, String> createMap() {
+        Map<String, String> result = new HashMap<>();
+        result.put(".", "DOTCODE");
+        result.put("1", "CODE1");
+        result.put(";", "MERGED_COUPON");
+        result.put("<", "CODE39_BASE32, CODE32, ITALIAN PHARMACODE, PARAF, LABELCODE_V");
+        result.put(">", "LABELCODE_IV");
+        result.put("=", "TRIOPTIC");
+        result.put("?", "KOREA_POST");
+        result.put(",", "INFOMAIL");
+        result.put("`", "EAN13_ISBN");
+        result.put("[", "SWEEDISH_POST");
+        result.put("|", "RM_MAILMARK");
+        result.put("]", "BRAZIL_POST");
+        result.put("A", "AUS_POST");
+        result.put("B", "BRITISH_POST");
+        result.put("C", "CANADIAN_POST");
+        result.put("D", "EAN8");
+        result.put("E", "UPCE");
+        result.put("G", "BC412");
+        result.put("H", "HAN_XIN_CODE");
+        result.put("I", "GS1_128");
+        result.put("J", "JAPAN_POST");
+        result.put("K", "KIX_CODE");
+        result.put("L", "PLANET_CODE");
+        result.put("M", "USPS_4_STATE, INTELLIGENT_MAIL");
+        result.put("N", "UPU_4_STATE, ID_TAGS");
+        result.put("O", "OCR");
+        result.put("P", "POSTNET");
+        result.put("Q", "HK25, CHINA_POST");
+        result.put("R", "MICROPDF");
+        result.put("S", "SECURE_CODE");
+        result.put("T", "TLC39");
+        result.put("U", "ULTRACODE");
+        result.put("V", "CODABLOCK_A");
+        result.put("W", "POSICODE");
+        result.put("X", "GRID_MATRIX");
+        result.put("Y", "NEC25");
+        result.put("Z", "MESA");
+        result.put("a", "CODABAR");
+        result.put("b", "CODE39");
+        result.put("c", "UPCA");
+        result.put("d", "EAN13");
+        result.put("e", "I25");
+        result.put("f", "S25 (2BAR and 3BAR)");
+        result.put("g", "MSI");
+        result.put("h", "CODE11");
+        result.put("i", "CODE93");
+        result.put("j", "CODE128");
+        result.put("k", "UNUSED");
+        result.put("l", "CODE49");
+        result.put("m", "M25");
+        result.put("n", "PLESSEY");
+        result.put("o", "CODE16K");
+        result.put("p", "CHANNELCODE");
+        result.put("q", "CODABLOCK_F");
+        result.put("r", "PDF417");
+        result.put("s", "QRCODE");
+        result.put("t", "TELEPEN");
+        result.put("u", "CODEZ");
+        result.put("v", "VERICODE");
+        result.put("w", "DATAMATRIX");
+        result.put("x", "MAXICODE");
+        result.put("y", "RSS, GS1_DATABAR, COMPOSITE");
+        result.put("z", "AZTEC_CODE");
+        result.put("-", "MICROQR_ALT");
+        result.put("{", "GS1_DATABAR_LIM");
+        result.put("}", "GS1_DATABAR_EXP");
+
+        return Collections.unmodifiableMap(result);
+    }
+
+    private static String getSymbol(String codeId) {
+        if (symbologyCodeMapper.containsKey(codeId)) {
+            return symbologyCodeMapper.get(codeId);
+        }
+        return "UNDEFINED";
+    }
 
     private static final String BARCODE_READ_SUCCESS = "barcodeReadSuccess";
     private static final String BARCODE_READ_FAIL = "barcodeReadFail";
@@ -76,6 +157,7 @@ public class HoneywellScannerModule extends ReactContextBaseJavaModule implement
             params.putString("timeStamp", barcodeReadEvent.getTimestamp());
             params.putString("charSet", barcodeReadEvent.getCharset().displayName());
             params.putString("codeId", barcodeReadEvent.getCodeId());
+            params.putString("symbology", getSymbol(barcodeReadEvent.getCodeId()));
         }
         params.putString("propagated", "true");
         sendEvent(BARCODE_READ_SUCCESS, params);
